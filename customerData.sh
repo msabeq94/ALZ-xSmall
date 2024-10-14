dae6fdb490069169a99b94b0aaa99b6238255fac
msabeq94 <msabeq@outlook.com>
2024-10-14 15:39:25 +0300

    Update customerData.sh and managementGroups.parameters.XSmall.json

--- customerData.sh
+++ customerData.sh
@@ -7,18 +7,18 @@ rg=rg-management
 
 
 
-sed -i "s/PPCR/N/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
+#sed -i "s/PPCR/N/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
 # Prompt the user for deploymentType 
-echo "Enter the AZL deployment Type (Small, Medium or Large)"
+echo "Enter the AZL deployment Type (XSmall,Small, Medium or Large)"
 read deploymentType
 
 # Prompt the user for customer Location 
 echo "Enter customer Location (Eg uksouth):"
 read LOCATION
-
+sed -i "s/customerLocation/${LOCATION}/g" ResourceGroup.parameters.XSmall.json
 # Feed files
-sed -i "s/customerLocation/${LOCATION}/g" infra-as-code/bicep/modules/logging/parameters/logging.parameters.all.json
-sed -i "s/customerLocation/${LOCATION}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
+#sed -i "s/customerLocation/${LOCATION}/g" infra-as-code/bicep/modules/logging/parameters/logging.parameters.all.json
+#sed -i "s/customerLocation/${LOCATION}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
 
 # Prompt the user for customer Name 
 echo "Enter customer Name:"
@@ -26,21 +26,21 @@ read customerName
 
 # Feed files
 
-sed -i "s/customerName/${customerName}/g" infra-as-code/bicep/modules/managementGroups/parameters/managementGroups.parameters.$deploymentType.json
+sed -i "s/customerName/${customerName}/g" managementGroups.parameters.XSmall.json
 
 # Prompt the user for mgmt subscirption Id 
 echo "Enter customer management subscription ID:"
 read mgmtSubID
 
 # Feed file
-sed -i "s/mgmtSubID/${mgmtSubID}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
+#sed -i "s/mgmtSubID/${mgmtSubID}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
 
 # Prompt the user for the securityEmail variable 
-echo "Enter customer Security Email:"
-read securityEmail
+#echo "Enter customer Security Email:"
+#read securityEmail
 
 # Feed files
-sed -i "s/securityEmail/${securityEmail}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
+#sed -i "s/securityEmail/${securityEmail}/g" infra-as-code/bicep/modules/policy/assignments/alzDefaults/parameters/alzDefaultPolicyAssignments.parameters.all.json
 
 # Output the entered variables
 echo "#############################################################"
@@ -50,4 +50,4 @@ echo "Location: $LOCATION"
 echo "Management Sub ID: $mgmtSubID"
 echo "Deployment Type: $deploymentType"
 echo "Customer Name: $customerName"
-echo "Security Email: $securityEmail"
+#echo "Security Email: $securityEmail"
