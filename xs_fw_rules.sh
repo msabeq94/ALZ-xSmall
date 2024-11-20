a01ce8999e186531e368c3ebf3d0b6ff4a51f465
msabeq94 <msabeq@outlook.com>
2024-11-20 16:44:07 +0200

    Add Network Rule Collection configuration commands

--- xs_fw_rules.sh
+++ xs_fw_rules.sh
@@ -22,11 +22,70 @@ read -p "Enter your choice (1, 2, or 3): " choice
 case $choice in
   1)
     echo "You selected NAT Rule Collection."
-    # Add your NAT Rule Collection configuration commands here
+   
     ;;
   2)
     echo "You selected Network Rule Collection."
-    # Add your Network Rule Collection configuration commands here
+
+    echo "Enter the Network Rule Collection name:"
+    read NetworkCollectionName
+
+    echo "Enter the Network Rule Collection priority (100-64999):"
+    read  NetworkCollectionPriority
+
+    echo "Select the action: (1) Allow, (2) Deny"
+    read -p "Enter your choice (1 or 2): " choice
+
+    # Respond based on user input
+    case $choice in
+        1)
+            action="Allow"
+            ;;
+        2)
+            action="Deny"
+            ;;
+        *)
+            echo "Invalid choice. Please enter 1 or 2."
+            exit 1
+            ;;
+    esac
+
+    
+    echo "Enter the Rule Name:"
+    read NetworkRuleName
+
+    echo "Select the protocol: (1) Any, (2) TCP, (3) UDP"
+    read -p "Enter your choice : " choice
+
+    case $protocol in
+        1)
+            protocol="Any"
+            ;;
+        2)
+            protocol="TCP"
+            ;;
+        3)
+            protocol="UDP"
+            ;;
+        *)
+            echo "Invalid protocol. Please enter 1, 2, or 3."
+            exit 1
+            ;;
+    esac
+
+    echo "Enter the source address:"    
+    read  NetworksourceAddress
+
+    echo "Enter the destination address:"
+    read  NetworkdestinationAddress
+
+    echo "Enter the destination port:"
+    read  NetworkdestinationPort
+
+
+ az network firewall network-rule create  --collection-name $NetworkCollectionName --firewall-name $firewallName --name $NetworkRuleName --protocols $protocol --resource-group $rg   --source-addresses $NetworksourceAddress  --destination-addresses $NetworkdestinationAddress --destination-ports $NetworkdestinationPort --action $action --priority $NetworkCollectionPriority
+
+
     ;;
   3)
     echo "You selected Application Rule Collection."
@@ -40,14 +99,14 @@ esac
 ##
 
 
-# #network-rule
-# az network firewall network-rule create  --collection-name "DefweererwewausdsdsdltCollection" --firewall-name $firewallName --name "AllowewwAll" --protocols "Any" --resource-group $rg   --source-addresses "10.0.0.0/24"  --destination-addresses "10.1.0.0/24" --destination-ports "*" --action "Allow" --priority 3550 
+# # #network-rule
+#  az network firewall network-rule create  --collection-name "DefweererwewausdsdsdltCollection" --firewall-name $firewallName --name "AllowewwAll" --protocols "Any" --resource-group $rg   --source-addresses "10.0.0.0/24"  --destination-addresses "10.1.0.0/24" --destination-ports "*" --action "Allow" --priority 3550 
 
-# #nat-rule
+# # #nat-rule
 # az network firewall nat-rule create --collection-name "DefaultCollectionSSS" --firewall-name $firewallName --name "AllowAll" --protocols "Any" --resource-group $rg --source-addresses "*" --destination-addresses $fwIPAddress --destination-ports "40" --action "Dnat" --priority 200 --translated-port 80 --translated-address "10.1.1.0"
 
-# #application-rule
-# #az network firewall application-rule create --collection-name "DefaultCollection" --firewall-name $firewallName --name "AllowAll" --protocols "Any" --resource-group $rg --source-addresses "*" --fqdn-tags "AzureBackup" --action "Allow" --priority 200
+# # #application-rule
+# # #az network firewall application-rule create --collection-name "DefaultCollection" --firewall-name $firewallName --name "AllowAll" --protocols "Any" --resource-group $rg --source-addresses "*" --fqdn-tags "AzureBackup" --action "Allow" --priority 200
 # az network firewall application-rule create --collection-name "DefaultCollection" --firewall-name $firewallName --name "AllowAll" --protocols "http=80" --resource-group $rg --source-addresses "*" --target-fqdns "www.alz.com" --action "Allow" --priority 200
 
 
